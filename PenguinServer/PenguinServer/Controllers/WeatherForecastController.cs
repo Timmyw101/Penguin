f5be03454f6870274c056428638cd869577c50b3
Timmyw101 <pokemonmastertimmy101@gmail.com>
2023-09-22 13:40:33 -0500

    small changes

--- PenguinServer/PenguinServer/Controllers/WeatherForecastController.cs
+++ PenguinServer/PenguinServer/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace PenguinServer.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Boiling"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -24,7 +24,7 @@ namespace PenguinServer.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = Random.Shared.Next(-40, 430),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
