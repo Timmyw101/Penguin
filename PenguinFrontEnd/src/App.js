3ba8e7793ca60a785a0366c7911a8b779c9c74bf
Jackson Levitt <jackson.levitt@beast-code.com>
2023-09-29 13:47:00 -0500

    f-cors

---
+++ PenguinFrontEnd/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  
+  const [text, setText] = useState("");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>
+            { text }
+          </p>
+        </div>
+        <Button onClick={GetMessages}>
+          Play Movie
+        </Button>
+      </header>
+    </div>
+  );
+
+  async function GetMessages(){
+
+    const response = await fetch("https://localhost:5294/Chat/messages");
+    const text = await response.text();
+    setText(text);
+  }
+  
+
+}
+
+
+function Button({ onClick, children }) {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default App;
--- PenguinServer/PenguinServer/Program.cs
+++ PenguinServer/PenguinServer/Program.cs
@@ -1,3 +1,6 @@
+using PenguinServer.Services;
+using PenguinServer.Services.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IChatService, ChatService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AnyOrigin", builder =>
+    {
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -19,5 +34,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AnyOrigin");
 
 app.Run();
